b42fb23ebec8fbca03556abe2e6336348a7dd35c
Trekker687 <113757673+Trekker687@users.noreply.github.com>
2024-10-25 17:52:29 +0530

    Add files via upload

---
+++ Variables/main.py
@@ -0,0 +1,8 @@
+x= "manvir"
+y= "aaaaaaaaaaaaaaaaaaah"
+z= 5
+print(x)
+print(y)
+print(z)
+username=input("Enter your username or log in is not possible ")
+print("Hello", username, "welcome to the platform")
